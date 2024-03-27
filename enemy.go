b22a91c98dbf04334cb31db5ca1818e653c6a287
Matthew Avallone <mva271@nyu.edu>
2024-03-26 23:22:17 -0400

    created enemy struct

---
+++ enemy.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/yohamta/ganim8/v2"
+)
+
+var (
+	random     = rand.New(rand.NewSource(time.Now().UnixNano()))
+	numEnemies = random.Intn(10) + 1 // add 1 to guarantee at least one enemy spawns
+
+)
+
+type gameEnemy struct {
+	x, y        int
+	moveDelay   int
+	orientation string
+
+	animInstance Animation
+	fly          Animation
+	attack       Animation
+	takeHit      Animation
+	death        Animation
+}
+
+func LoadEnemies() []*gameEnemy {
+	gameEnemies := make([]*gameEnemy, numEnemies)
+	for i := 0; i < numEnemies; i++ {
+		gameEnemies[i] = &gameEnemy{
+			x:           random.Intn(mapSize / 2),
+			y:           random.Intn(mapSize / 2),
+			moveDelay:   10, // Adjust this value to change the speed of the player
+			orientation: "S",
+		}
+		gameEnemies[i].generateSpawnCoordinates()
+		gameEnemies[i].LoadEnemyImage()
+	}
+	return gameEnemies
+}
+
+func (nme *gameEnemy) generateSpawnCoordinates() {
+	for gameMap[nme.x][nme.y] == 1 {
+		nme.x = random.Intn(mapSize / 2)
+		nme.y = random.Intn(mapSize / 2)
+	}
+}
+
+func (nme *gameEnemy) LoadEnemyImage() {
+	attackImg, _, _ := ebitenutil.NewImageFromFile("assets/Monsters_Creatures_Fantasy/Flying eye/Attack.png")
+	flyImg, _, _ := ebitenutil.NewImageFromFile("assets/Monsters_Creatures_Fantasy/Flying eye/Flight.png")
+	deathImg, _, _ := ebitenutil.NewImageFromFile("assets/Monsters_Creatures_Fantasy/Flying eye/Death.png")
+	takeHitImg, _, _ := ebitenutil.NewImageFromFile("assets/Monsters_Creatures_Fantasy/Flying eye/Take Hit.png")
+
+	gAttack := ganim8.NewGrid(150, 150, attackImg.Bounds().Dx(), attackImg.Bounds().Dy(), 0, 0, 1)
+	gFly := ganim8.NewGrid(150, 150, flyImg.Bounds().Dx(), flyImg.Bounds().Dy(), 0, 0, 1)
+	gDeath := ganim8.NewGrid(150, 150, deathImg.Bounds().Dx(), deathImg.Bounds().Dy(), 0, 0, 1)
+	gTakeHit := ganim8.NewGrid(150, 150, takeHitImg.Bounds().Dx(), takeHitImg.Bounds().Dy(), 0, 0, 1)
+
+	nme.fly.anim = ganim8.New(flyImg, gFly.Frames("1-8", "1"), 10*time.Millisecond)
+	nme.fly.originX = -0.4
+	nme.fly.originY = 1
+
+	nme.attack.anim = ganim8.New(attackImg, gAttack.Frames("1-8", "1"), 10*time.Millisecond)
+	nme.attack.originX = -0.4
+	nme.attack.originY = 0
+
+	nme.death.anim = ganim8.New(deathImg, gDeath.Frames("1-4", "1"), 10*time.Millisecond)
+	nme.death.originX = -0.4
+	nme.death.originY = 0
+
+	nme.takeHit.anim = ganim8.New(takeHitImg, gTakeHit.Frames("1-4", "1"), 10*time.Millisecond)
+	nme.takeHit.originX = -0.4
+	nme.takeHit.originY = 0
+
+	nme.animInstance = nme.fly
+}
