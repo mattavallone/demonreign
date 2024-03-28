3e6e42adc8db699e8585a5594b686087c7642903
Matthew Avallone <mva271@nyu.edu>
2024-03-28 14:32:56 -0400

    added map key

--- map.go
+++ map.go
@@ -2,6 +2,13 @@ package main
 
 const mapSize = 16
 
+/*
+* Map Key
+* 0 = free space
+* 1 = wall
+* 2 = enemy locations
+**/
+
 var gameMap = [][]int{
 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 	{0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0},
